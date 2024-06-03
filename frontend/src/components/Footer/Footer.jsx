4d90a51bfd53b3daca2a7a1ac22d9e6e04ff797b
zafrins <zafrin1412@gmail.com>
2024-06-03 14:20:05 +0600

    Testimonals of patients

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import logo from '../../assets/images/logo.png'
+// import 
 
 const Footer = () => {
   return (
